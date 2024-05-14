02f58a9df9b289866f6f46902b1cc556aa088103
Ivan Rybaruk <riv@exante.eu>
2024-05-14 17:06:26 +0300

    (setup) add css loader, add react

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,6 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
-import {BuildOptions} from "config/build/types/config";
+import {BuildOptions} from "./types/config";
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
 
